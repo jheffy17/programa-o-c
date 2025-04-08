8383a265226f5f41b058265bda118dc8a7e2f3bf
jheffy17 <jheffy.jhe17@gmail.com>
2025-04-08 00:04:30 +0000

    introdução a programação em c

---
+++ if2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>    
+   int main () {
+    
+     float temperatura = 29.0;
+
+     if ( temperatura >= 30) {
+     printf ("Está calor\n");
+     }
+
+
+
+   }
+        
+   
+    
---
+++ introduçao a programaçao em C/If.c
@@ -0,0 +1,17 @@
+#include <stdio.h>    
+
+        //entrada principal//
+        int main ()  {
+        
+       int idade = 18;
+
+       if(idade >= 28)  {
+            printf("Você é maior de idade\n");
+   
+        }
+        printf("Fora do if\n");
+    }
+  /* se a idade foi 18 ou  mais o programa acima deverá imprimir 'Você é maior de idade' */
+  /*se a pedida exigida não se cumprir e não tiver opções dentro do IF, logo só sera mostrado oque esta fora*/
+
+    
